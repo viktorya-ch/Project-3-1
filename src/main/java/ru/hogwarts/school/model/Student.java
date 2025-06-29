c5c73b24ce35961b26e82b7948cb662b394c6427
origin <viktoryaa2000@mail.ru>
2025-06-29 15:27:17 +0300

    new commit test 1

--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -2,6 +2,7 @@ package ru.hogwarts.school.model;
 
 import jakarta.persistence.*;
 
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -83,4 +84,17 @@ public class Student {
         this.avatar = avatar;
     }
 
+    @OneToMany(mappedBy = "student")
+    private List<Faculty> faculties;
+
+    public List<Faculty>getFaculties(){
+        return faculties;
+    }
+    public void setFaculties(List<Faculty>faculties){
+        this.faculties= faculties;
+    }
+
+    public Object getFaculty() {
+        return faculties;
+    }
 }
