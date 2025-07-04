a242575384ae4c940733b225ee16194d1f19add3
origin <viktoryaa2000@mail.ru>
2025-07-04 18:11:31 +0300

    HW 4.2

--- src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
+++ src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
@@ -6,5 +6,6 @@ import ru.hogwarts.school.model.Avatar;
 import java.util.Optional;
 
 public interface AvatarRepository extends JpaRepository<Avatar, Long> {
+
     Optional<Avatar>findByStudentID(Long studentID);
 }
